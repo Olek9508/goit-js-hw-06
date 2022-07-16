bedaca71412760a0d53000b545af0862806b7cc0
Oleksandr Volik <polskasasha@gmail.com>
2022-07-16 14:13:58 +0300

    Update-task-08-js

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,14 @@
-const formEls = document.querySelector("form");
+const formEls = document.querySelector('form');
 
-formEls.addEventListener("submit", onFillFildesInputRequired);
+formEls.addEventListener('submit', onFillFildesInputRequired);
 
 function onFillFildesInputRequired(event) {
   event.preventDefault();
   const email = event.currentTarget.email.value;
   const password = event.currentTarget.password.value;
 
-  email === "" || password === ""
-    ? alert("All fields must be filled out")
+  email === '' || password === ''
+    ? alert('All fields must be filled out')
     : console.log({
         email,
         password,
